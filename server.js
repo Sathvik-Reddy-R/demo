79ef349e27af95a24be416808ad43b492ed9306c
Sathvik-Reddy-R <regattesathvikreddy@gmail.com>
2026-04-30 11:29:26 +0530

    Update route handling and log message in server.js

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/:path(*)", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log("Server running on http://localhost:3000");
 });
